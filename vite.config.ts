ec4d00242f90117f5f550fafa09613418de2af24
JackalopeCode <22228272+jackalope-code@users.noreply.github.com>
2024-11-11 23:07:30 -0700

    VERY IMPORTANT BUILD CHANGES FOR VITE AND GH-PAGES

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "stream-countdown",
   plugins: [react()],
+  build: {
+    outDir: './build',
+    emptyOutDir: true
+  }
 })
