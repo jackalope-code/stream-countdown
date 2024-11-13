c845c53e76f5affdda721d8a70a905d8c9a95f02
JackalopeCode <22228272+jackalope-code@users.noreply.github.com>
2024-11-12 19:45:59 -0700

    Marquee almost done except for the 1px or whatever flicker

--- src/FirstStreamBackCountdown.tsx
+++ src/FirstStreamBackCountdown.tsx
@@ -1,5 +1,6 @@
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import Marquee from 'react-fast-marquee';
 
 function getFrogansFirstStreamBackDate() {
   const date = new Date();
@@ -14,13 +15,19 @@ const FirstStreamBackCountdown = () => {
   return (
     <>
       <div id="main-header">
-        <h1>Countdown to Frogan's return stream</h1>
+          <h1>Countdown to Frogan's return stream</h1>
+      </div>
+      <div id="banner">
+        <div id="banner-background">
+          <Marquee pauseOnHover={true} autoFill={true}>
+            <h2>Fr0gan returning to Twitch.tv | November 21st 5:30 PM PST / 8:30 PM EST | Go watch frogan | <a href="https://twitch.tv/frogan" target="_blank">twitch.tv/frogan</a> | Countdown until frogan is live |&nbsp;</h2>
+          </Marquee>
+        </div>
       </div>
       <div className="centered-content">
-        <h2>November 21st | 5:30 PM PST / 8:30 PM EST | twitch.tv/frogan</h2>
         <FlipClockCountdown to={getFrogansFirstStreamBackDate()}>
           {/* TODO: Put a message and a stream embed here */}
-          <p>Time's up!!! Go watch frogan!!!</p>
+          <p>Time's up!!! Go watch frogan at !!!</p>
         </FlipClockCountdown>
       </div>
     </>
