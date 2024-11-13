e74cc18761a7af1e0aa1d739934ba6ff0d655821
JackalopeCode <22228272+jackalope-code@users.noreply.github.com>
2024-11-13 12:50:25 -0700

    Mobile fixes

--- src/FirstStreamBackCountdown.tsx
+++ src/FirstStreamBackCountdown.tsx
@@ -1,19 +1,20 @@
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 import Marquee from 'react-fast-marquee';
+import MediaQuery from 'react-responsive';
 
 function getFrogansFirstStreamBackDate() {
-  const date = new Date();
+  let date = new Date();
   date.setUTCMonth(10);
   date.setUTCDate(22);
   date.setUTCHours(1, 30, 0);
-  console.log(date);
+  console.log("CALCULATED START DATE/TIME IN LOCAL TIMEZONE: " + date.toDateString() + " " + date.toLocaleTimeString("en-US", {hour12: true}));
   return date;
 }
 
 const FirstStreamBackCountdown = () => {
-  const twitchPageMockLink = <span className="mock-link">twitch.tv/frogan</span>
-  // const twitchPageLink = <a href="https://twitch.tv/frogan" target="_blank">twitch.tv/frogan</a>;
+  const froganPageMockLink = <span className="mock-link">twitch.tv/frogan</span>
+  const froganPageLink = <a href="https://twitch.tv/frogan" target="_blank">twitch.tv/frogan</a>;
 
   return (
     <>
@@ -24,15 +25,25 @@ const FirstStreamBackCountdown = () => {
         <div id="banner-container">
           <div id="banner-background"/>
           <Marquee pauseOnHover={true} autoFill={false}>
-            <div id="marquee-scroll-text">Fr0gan returning to Twitch | {twitchPageMockLink} | November 21st 5:30 PM PST / 8:30 PM EST | Go watch frogan then | {twitchPageMockLink} | Countdown to frogan's live return | Fr0gan will be returning to Twitch live November 21st 5:30 PM PST / 8:30 PM EST |&nbsp;</div>
+            <div id="marquee-scroll-text">Fr0gan returning to Twitch | {froganPageMockLink} | November 21st 5:30 PM PST / 8:30 PM EST | Go watch frogan then | {froganPageMockLink} | Countdown to frogan's live return | Fr0gan will be returning to Twitch live November 21st 5:30 PM PST / 8:30 PM EST |&nbsp;</div>
           </Marquee>
         </div>
       </div>
       <div className="centered-content">
-        <FlipClockCountdown to={getFrogansFirstStreamBackDate()}>
-          {/* TODO: Put a message and a stream embed here */}
-          <p>Time's up!!! Go watch frogan at !!!</p>
-        </FlipClockCountdown>
+        <MediaQuery query="(max-device-width: 520px">
+          <FlipClockCountdown to={getFrogansFirstStreamBackDate()} renderMap={[true, false, false, false]} showSeparators={false}>
+          </FlipClockCountdown>
+          <FlipClockCountdown to={getFrogansFirstStreamBackDate()} renderMap={[false, true, true, true]} digitBlockStyle={{fontSize: "1.5rem", width: "23px", height: "40px"}}>
+            {/* TODO: Put a message and a stream embed here */}
+            <p>Time's up!!! Go watch frogan at {froganPageLink}!!!</p>
+          </FlipClockCountdown>
+        </MediaQuery>
+        <MediaQuery query="(min-device-width: 521px">
+          <FlipClockCountdown to={getFrogansFirstStreamBackDate()}>
+            {/* TODO: Put a message and a stream embed here */}
+            <p>Time's up!!! Go watch frogan at {froganPageLink}!!!</p>
+          </FlipClockCountdown>
+        </MediaQuery>
       </div>
     </>
   )
