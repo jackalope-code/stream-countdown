0c29c07b5bc9c9f305d844a8f50591ed08e6e800
JackalopeCode <22228272+jackalope-code@users.noreply.github.com>
2024-11-12 20:20:14 -0700

    Barbie theme looking good. Dark color added to it. Extra verbage added to marque. Page title changed.

--- src/FirstStreamBackCountdown.tsx
+++ src/FirstStreamBackCountdown.tsx
@@ -12,15 +12,19 @@ function getFrogansFirstStreamBackDate() {
 }
 
 const FirstStreamBackCountdown = () => {
+  const twitchPageMockLink = <span className="mock-link">twitch.tv/frogan</span>
+  // const twitchPageLink = <a href="https://twitch.tv/frogan" target="_blank">twitch.tv/frogan</a>;
+
   return (
     <>
       <div id="main-header">
           <h1>Countdown to Frogan's return stream</h1>
       </div>
-      <div id="banner">
-        <div id="banner-background">
-          <Marquee pauseOnHover={true} autoFill={true}>
-            <h2>Fr0gan returning to Twitch.tv | November 21st 5:30 PM PST / 8:30 PM EST | Go watch frogan | <a href="https://twitch.tv/frogan" target="_blank">twitch.tv/frogan</a> | Countdown until frogan is live |&nbsp;</h2>
+      <div id="banner-wrapper">
+        <div id="banner-container">
+          <div id="banner-background"/>
+          <Marquee pauseOnHover={true} autoFill={false}>
+            <div id="marquee-scroll-text">Fr0gan returning to Twitch.tv | {twitchPageMockLink} | November 21st 5:30 PM PST / 8:30 PM EST | Go watch frogan then | {twitchPageMockLink} | Countdown to frogan's live return | Fr0gan will be returning to Twitch.tv live November 21st 5:30 PM PST / 8:30 PM EST |&nbsp;</div>
           </Marquee>
         </div>
       </div>
